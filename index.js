a1d18df2c39e18434cbb319ce4749b6c26b304c7
Starry Knight <82652543+astarryknight@users.noreply.github.com>
2024-04-19 19:27:33 +0000

    add preliminary flood flow algorithm and continued styling

--- index.js
+++ index.js
@@ -55,13 +55,13 @@ function generateBoxes(){
     var ctr=0;
     for(i=0;i<game.length;i++){
         row = document.createElement("div");
-        row.id=rid;
+        row.id="r"+rid;
         row.classList.add("row");
         document.getElementById("main").appendChild(row);
         id=0;
         for(j=0;j<game[i].length;j++){
             box = document.createElement("div");
-            box.id=id;
+            box.id=(rid*8)+id;
             box.classList.add("box");
             if(ctr%2==0){
                 box.classList.add("b");
@@ -79,19 +79,60 @@ function generateBoxes(){
 }
 
 function click(e){
-    var x=e.target.id;
-    var y=e.target.parentElement.id;
-    e.target.textContent=game[x][y];
+    showBox(e.target);
+    calculateBoxes(e.target);
 }
 
-function showBox(x,y){
-    document.getElementById()
+function showBox(el){
+    var x=Number(el.id%8);
+    var y=Number((el.id-(el.id%8))/8);
+    
+    console.log(el.id, x, y);
+    if(game[y][x]!=0){
+        el.textContent=game[y][x];
+    }
+    el.classList.add("show");
 }
 
-function showBoxes(x,y){
-    g=true;
-    while(g){
+//dumbbbb
+function contains(array, array2, param){
+    for(i=0;i<array.length;i++){
+      if(array[i][0]==param[0]&&array[i][1]==param[1]){ return true; }
+    }
+    for(i=0;i<array2.length;i++){
+        if(array2[i][0]==param[0]&&array2[i][1]==param[1]){ return true; }
+    }
+    return false;
+}
+  
 
+function calculateBoxes(start){
+    var explored=[];
+    var temp=[Number((start.id%8)), Number((start.id-(start.id%8))/8)]
+    console.log(temp[0], temp[1]);
+    if(game[temp[1]][temp[0]]==0){
+        var waiting=[[temp[0], temp[1]]];
+        while(waiting.length>0){
+            var current=waiting.pop();
+            
+            var i=current[0];
+            var j=current[1];
+            if((i-1)>=0 && (j+1)<8 && game[j+1][i-1]==0 && !contains(explored, waiting, ([i-1, j+1]))){ waiting.unshift([i-1, j+1]); } 
+            if((j+1)<8 && game[j+1][i]==0 && !contains(explored, waiting, ([i, j+1]))){ waiting.unshift([i, j+1]); } 
+            if((i+1)<8 && (j+1)<8 && game[j+1][i+1]==0 && !contains(explored, waiting, ([i+1, j+1]))){ waiting.unshift([i+1, j+1]); }
+            if((i-1)>=0 && game[j][i-1]==0 && !contains(explored, waiting, ([i-1, j]))){ waiting.unshift([i-1, j]); }
+            if((i+1)<8 && game[j][i+1]==0 && !contains(explored, waiting, ([i+1, j]))){ waiting.unshift([i+1, j]); }
+            if((i-1)>=0 && (j-1)>=0 && game[j-1][i-1]==0 && !contains(explored, waiting, ([i-1, j-1]))){ waiting.unshift([i-1, j-1]); }
+            if((j-1)>=0 && game[j-1][i]==0 && !contains(explored, waiting, ([i, j-1]))){ waiting.unshift([i, j-1]); }
+            if((i+1)<8 && (j-1)>=0 && game[j-1][i+1]==0 && !contains(explored, waiting, ([i+1, j-1]))){ waiting.unshift([i+1, j-1]); }
+
+            explored.push(current);
+            //debugger;
+        }
+        console.log(explored);
+        for(i=0;i<explored.length;i++){
+            showBox(document.getElementById((explored[i][1]*8)+explored[i][0]));
+        }
     }
 }
 
